127137f17c26dac843e133b4f550d5822bd669bb
Yanmingg <yanmingg@student.unimelb.edu.au>
2021-10-03 20:54:11 +0800

    add to main

--- IT_Java/src/CRM.java
+++ IT_Java/src/CRM.java
@@ -15,19 +15,22 @@ public class CRM {
         Contact contact1 = new Contact("Yanming", "yanmingg@student.unimelb.edu.au");
         Contact contact2 = new Contact("Nobody", "abc@gmail.com");
         Contact contact3 = new Contact("Yanming2", "jim15806179082@gmail.com");
+        Contact contact4 = new Contact("TianyuHuang", "thhuang@student.unimelb.edu.au");
         user1.getContactPage().newContact(contact1);
         user1.getContactPage().newContact(contact2);
         user1.getContactPage().newContact(contact3);
-        importer.loadCSV(user1.getContactPage().getContactList(),user1.getSchedulePage().getScheduleList(), filename);
+        user1.getContactPage().newContact(contact4);
+       // importer.loadCSV(user1.getContactPage().getContactList(),user1.getSchedulePage().getScheduleList(), filename);
         System.out.println(user1.getSchedulePage().getScheduleList());
 
         System.out.println(user1.getSchedulePage().sortName());
         System.out.println(user1.getSchedulePage().searchName("beeting3"));
         //user1.getContactPage().sendEmail(contact2,contact1,"Test", "TestEmail");
         System.out.println(user1.getContactPage().getContactList());
-        user1.sendEmailToContact(contact1, "Testing by user 10:19", "Test!");
+        user1.sendEmailToContact(contact1, "Testing by user 10", "Test!");
         user1.addEmailContacts(contact1);
         user1.addEmailContacts(contact3);
+        user1.addEmailContacts(contact4);
         user1.sendEmailToContacts(user1.getEmailContact(),"Testing for sending emails", "Testing");
     }
 }
