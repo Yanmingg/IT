4f02d7c471e3a098a77a1e9908e9202f5d054bd9
Quanying LYU <quanying.lyu@student.unimelb.edu.au>
2021-10-03 16:38:07 +0800

    project2-true project

---
+++ project2/src/main/java/com/example/crm/controller/TaskController.java
@@ -0,0 +1,53 @@
+package com.example.crm.controller;
+
+import com.example.crm.entity.Contact;
+import com.example.crm.entity.Task;
+import com.example.crm.repository.ContactRepository;
+import com.example.crm.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("/task")
+public class TaskController {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @GetMapping("/findAll")
+    public List<Task> findAll(){
+        return taskRepository.findAll();
+    }
+
+    @PostMapping("/save")
+    public String save(@RequestBody Task task){
+        Task result = taskRepository.save(task);
+        if(result!=null){
+            return "success";
+        }else{
+            return "error";
+        }
+    }
+
+    @GetMapping("/findid/{taskid}")
+    public Task findId(@PathVariable("taskid") Integer id){
+        taskRepository.findById(id);
+        return taskRepository.getById(id);
+    }
+
+    @DeleteMapping("/deleteid/{id}")
+    public void deleteId(@PathVariable("id") Integer id){
+        taskRepository.deleteById(id);
+    }
+
+    @PutMapping("/update")
+    public String update(@RequestBody Task task){
+        Task result = taskRepository.save(task);
+        if(result!=null){
+            return "success";
+        }else{
+            return "error";
+        }
+    }
+
+}
